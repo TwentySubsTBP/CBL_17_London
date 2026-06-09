8bd11f5649ec2a32c8bcc5eb1aec00fd6c020fd0
edenvalk <edenvalk@gmail.com>
2026-06-09 13:19:02 +0200

    Test duplicate forces

--- main/src/observation_chance.py
+++ main/src/observation_chance.py
@@ -4,6 +4,9 @@ import geopandas as gpd
 import plotly.express as px
 import json
 
+# Stop and search represents police activity
+# Source: https://www.gov.uk/government/publications/police-powers-and-procedures-in-england-and-wales-201112-user-guide/user-guide-to-police-powers-and-procedures#stop-and-search-1
+
 data_directory = Path("main\data\stop_and_search_23_26_10_forces")
 
 target_forces = {
@@ -24,10 +27,10 @@ data_frames = []
 for map in data_directory.iterdir():
     for file in map.iterdir():
         force = file.stem[8:].replace("-stop-and-search", "")
-        print(force)
+        #print(force)
         if force not in target_forces:
             continue
-        print(f'Loading: {file.name}')
+        #print(f'Loading: {file.name}')
         df = pd.read_csv(file)
         df["Force"] = force
         data_frames.append(df)
@@ -35,37 +38,25 @@ if not data_frames:
     raise ValueError("No stop and search files found.")
 
 stop_and_search_df = pd.concat(data_frames, ignore_index = True)
-print(f"Rows: {len(stop_and_search_df):,}")
-print(f"Columns: {len(stop_and_search_df.columns)}")
-
-df = stop_and_search_df[[
-    "Date",
-    "Latitude",
-    "Longitude",
-    "Force"
-]]
-
-print(df.shape)
+# print(f"Rows: {len(stop_and_search_df):,}")
+# print(f"Columns: {len(stop_and_search_df.columns)}")
 
 #################################
-before = len(df)
-df["Longitude"] = pd.to_numeric(df["Longitude"], errors="coerce")
-df["Latitude"] = pd.to_numeric(df["Latitude"], errors="coerce")
-df = df.dropna(subset=["Longitude", "Latitude"]).copy()
-after = len(df)
-print("Deleted:", before - after)
+before = len(stop_and_search_df)
+stop_and_search_df["Longitude"] = pd.to_numeric(stop_and_search_df["Longitude"], errors="coerce")
+stop_and_search_df["Latitude"] = pd.to_numeric(stop_and_search_df["Latitude"], errors="coerce")
+stop_and_search_df = stop_and_search_df.dropna(subset=["Longitude", "Latitude"]).copy()
+after = len(stop_and_search_df)
+print("Deleted:", round(((before - after)/before)*100, 1), "%", '\n')
 
 gdf = gpd.GeoDataFrame(
-    df,
-    geometry=gpd.points_from_xy(df["Longitude"], df["Latitude"]),
+    stop_and_search_df,
+    geometry=gpd.points_from_xy(stop_and_search_df["Longitude"], stop_and_search_df["Latitude"]),
     crs="EPSG:4326"
 )
-print(gdf.head())
 
 # from https://www.data.gov.uk/dataset/6785cb41-914a-444f-9a54-7981171132a3/lower-layer-super-output-areas-december-2021-boundaries-ew-bsc-v4-and-rural-urban-classification
 lsoa_boundaries = gpd.read_file(r"main\src\LSOA_(2021)_EW_BSC_V4_to_Rural_Urban_Classification.geojson")
-print(lsoa_boundaries.crs)
-print(gdf.crs)
 
 gdf = gpd.sjoin(gdf, lsoa_boundaries, how="left", predicate="within")
 gdf = gdf[[
@@ -74,26 +65,59 @@ gdf = gdf[[
     'LSOA21CD',
     'LSOA21NM',
     'LSOA21NMW',
+    'Outcome linked to object of search',
 ]]
-print(gdf.head())
-print(gdf.columns)
 
-counts = gdf.groupby("LSOA21CD").size().reset_index(name="n_crimes")
-print(counts.head(), counts.shape)
+print("Joined gdf for exposure estimation:")
+gdf["Month"] = pd.to_datetime(gdf["Date"]).dt.to_period("M")
+print(gdf.head(5), '\n')
+
+########################################
+# Estimate exposure
+police_activity_lsoa_month = gdf.groupby(['LSOA21CD', 'Force', 'Month']).size().reset_index(name = "n_police_activity")
+print("Police activity per LSOA per Month:")
+print(police_activity_lsoa_month.head(), '\n')
+
+duplicate_forces = {}
+for _, row in police_activity_lsoa_month.iterrows():
+    lsoa = row["LSOA21CD"]
+    force = row["Force"]
+    if lsoa not in duplicate_forces:
+        duplicate_forces[lsoa] = []
+        duplicate_forces[lsoa].append(force)
+    else:
+        if force not in duplicate_forces[lsoa]:
+            duplicate_forces[lsoa].append(force)
+
+count_duplicates = 0
+print("LSOA's with multiple forces:")
+for lsoa, forces in duplicate_forces.items():
+    if len(forces) > 1:
+        count_duplicates += 1
+        # print(lsoa, forces, '\n')
+
+multiple_forces_percentage = round((count_duplicates / len(duplicate_forces))* 100, 1)
+print("Percentage of LSOA's with multiple forces", multiple_forces_percentage, '%', '\n')
+
+#######################################
+
+counts = gdf.groupby("LSOA21CD").size().reset_index(name="n_patroulle")
+
+print("Count dataframe (police activity count per LSOA):")
+print(counts.head(), counts.shape, counts["n_patroulle"].max())
+
 lsoa_active = lsoa_boundaries.merge(counts, on="LSOA21CD", how="inner")
 geojson = json.loads(lsoa_active.to_json())
-
 fig = px.choropleth_mapbox(
     lsoa_active,
     geojson=geojson,
     locations="LSOA21CD",
     featureidkey="properties.LSOA21CD",
-    color="n_crimes",
+    color="n_patroulle",
     color_continuous_scale="OrRd",
     mapbox_style="carto-positron",
     zoom=9,
     center={"lat": 51.5, "lon": -0.1},
     opacity=0.6
 )
-
-fig.show()
+#fig.show()
